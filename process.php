fc41fb7c2ab973820f28ec68840117f3f441a240
root <sumithran@india.com>
2018-10-03 20:22:06 +0530

    Changes not staged for commit: 	deleted:    CManager 	modified:   checklogin.php 	modified:   composer.json 	modified:   composer.lock 	modified:   connect.php 	modified:   signup.php 	modified:   task.php 	modified:   vendor/composer/autoload_psr4.php 	modified:   vendor/composer/autoload_real.php

---
+++ process.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'connect.php';
+//Fetching Values from URL  
+$usrid = $_POST['usrid'];
+$topid = $_POST['topid'];
+$cstat = $_POST['cstat'];
+
+//Insert query 
+
+if (isset($cstat AND !empty($cstat))) {
+	if ($cstat=='ON') {
+		$sql = "UPDATE `task_reg` SET `payment`='1' WHERE (`top_id`='".$topid."' AND `use_id`='".$usrid."') ";
+	}elseif ($cstat=='OFF') {
+		$sql = "UPDATE `task_reg` SET `payment`='0' WHERE (`top_id`='".$topid."' AND `use_id`='".$usrid."') ";
+	}
+}
+
+
+
+
+if(mysqli_query($link,$sql)){
+	echo "Working good";
+}else {
+	echo "error";
+	echo $sql." ". mysqli_error($link);
+}
+
+// clossing connection
+mysqli_close($link);
+https://launchpad.net/onboardCreate your macros here.
